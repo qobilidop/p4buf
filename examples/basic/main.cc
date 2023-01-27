22ef1f090465bda59df5b7ab5e6484198635dd01
Bili Dong <qobilidop@gmail.com>
2023-01-27 10:19:33 +0000

    Demonstrate tuple type in example

--- examples/basic/main.cc
+++ examples/basic/main.cc
@@ -17,12 +17,16 @@ int main(int argc, char* argv[]) {
   // Specify a P4 type.
   // clang-format off
   P4Type p4type(P4StructT{  // struct {
-    {"s0", P4StructT{       //   struct {
-      {"a", P4BitT{1}},     //     bit<1> a;
-      {"b", P4BitT{2}},     //     bit<2> b;
-    }},                     //   } s0;
-    {"c", P4BitT{3}},       //   bit<3> c;
-    {"d", P4BitT{4}},       //   bit<4> d;
+    {"a", P4BitT{1}},       //   bit<1> a;
+    {"b", P4BitT{2}},       //   bit<2> b;
+    {"s", P4StructT{        //   struct {
+      {"c", P4BitT{3}},     //     bit<3> c;
+      {"d", P4BitT{4}},     //     bit<4> d;
+    }},                     //   } s;
+    {"t", P4TupleT{         //   tuple<
+      P4BitT{5},            //     bit<5>,
+      P4BitT{6},            //     bit<6>,
+    }},                     //   > t;
   });                       // }
   // clang-format on
 
@@ -31,14 +35,16 @@ int main(int argc, char* argv[]) {
   P4Data p4data(p4type, 0);
 
   // Edit named fields.
-  p4data["/s0/a"] = uint8_t{1};
-  p4data["/s0/b"] = uint8_t{1};
-  p4data["/c"] = uint8_t{1};
-  p4data["/d"] = p4data["/c"];
+  p4data["/a"] = uint8_t{1};
+  p4data["/b"] = uint8_t{1};
+  p4data["/s/c"] = uint8_t{1};
+  p4data["/s/d"] = p4data["/a"];
+  p4data["/t/0"] = uint8_t{1};
+  p4data["/t/1"] = p4data["/b"];
 
   // Now the underlying buffer becomes:
-  // 10100100 01000000
-  // abbcccdd dd
+  // 10100100 01000010 00001000
+  // abbcccdd dd.t[0]. .t[1]
   PrintBuffer(*p4data.buffer());
 
   return 0;
